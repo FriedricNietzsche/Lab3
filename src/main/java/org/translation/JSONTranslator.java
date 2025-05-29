50a7b4fa0053449032c2c1f1b541932b7dc173f4
FriedricNietzsche <krisviraujla@gmail.com>
2025-05-29 15:52:54 -0400

    Finished JSONTranslator and passed all 3 test from the test cases

--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * An implementation of the Translator interface which reads in the translation
@@ -15,7 +15,10 @@ import org.json.JSONArray;
  */
 public class JSONTranslator implements Translator {
 
-    // TODO Task: pick appropriate instance variables for this class
+    // Map from country code (alpha-3) to list of available language codes
+    private final Map<String, List<String>> countryToLanguageCodes = new HashMap<>();
+    // Map from country code to (language code -> translated name)
+    private final Map<String, Map<String, String>> countryToTranslation = new HashMap<>();
 
     /**
      * Constructs a JSONTranslator using data from the sample.json resources file.
@@ -30,39 +33,55 @@ public class JSONTranslator implements Translator {
      * @throws RuntimeException if the resource file can't be loaded properly
      */
     public JSONTranslator(String filename) {
-        // read the file to get the data to populate things...
         try {
-
-            String jsonString = Files.readString(Paths.get(getClass().getClassLoader().getResource(filename).toURI()));
+            String jsonString = Files.readString(
+                    Paths.get(getClass().getClassLoader().getResource(filename).toURI())
+            );
 
             JSONArray jsonArray = new JSONArray(jsonString);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject obj = jsonArray.getJSONObject(i);
+                String countryCode = obj.getString("alpha3");
 
-            // TODO Task: use the data in the jsonArray to populate your instance variables
-            //            Note: this will likely be one of the most substantial pieces of code you write in this lab.
+                Map<String, String> translations = new HashMap<>();
+                List<String> codes = new ArrayList<>();
 
-        }
-        catch (IOException | URISyntaxException ex) {
-            throw new RuntimeException(ex);
+                // Iterate over all keys, skipping metadata fields
+                for (String key : obj.keySet()) {
+                    if ("id".equals(key) || "alpha2".equals(key) || "alpha3".equals(key)) {
+                        continue;
+                    }
+                    String translatedName = obj.getString(key);
+                    translations.put(key, translatedName);
+                    codes.add(key);
+                }
+
+                countryToTranslation.put(countryCode, translations);
+                countryToLanguageCodes.put(countryCode, codes);
+            }
+        } catch (IOException | URISyntaxException ex) {
+            throw new RuntimeException("Failed to load translation data from " + filename, ex);
         }
     }
 
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TODO Task: return an appropriate list of language codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        List<String> codes = countryToLanguageCodes.get(country);
+        return codes != null ? new ArrayList<>(codes) : Collections.emptyList();
     }
 
     @Override
     public List<String> getCountries() {
-        // TODO Task: return an appropriate list of country codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>();
+        return new ArrayList<>(countryToLanguageCodes.keySet());
     }
 
     @Override
     public String translate(String country, String language) {
-        // TODO Task: complete this method using your instance variables as needed
+        Map<String, String> translations = countryToTranslation.get(country);
+        if (translations != null) {
+            return translations.get(language);
+        }
         return null;
     }
 }
+
